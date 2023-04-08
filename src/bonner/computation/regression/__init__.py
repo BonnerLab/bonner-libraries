1d1316aedd13e871813e773a17d9eeca34be11b0
Ray Chen <elfynray@hotmail.com>
2023-04-08 00:38:47 -0400

    init

--- src/bonner/computation/regression/__init__.py
+++ src/bonner/computation/regression/__init__.py
@@ -1,12 +1,14 @@
 __all__ = [
     "Regression",
     "LinearRegression",
+    "PLSRegression",
     "regression",
     "regression_cv",
 ]
 
 from bonner.computation.regression._definition import Regression
 from bonner.computation.regression._linear_regression import LinearRegression
+from bonner.computation.regression._pls_regression import PLSRegression
 from bonner.computation.regression._utilities import (
     regression,
     regression_cv,
