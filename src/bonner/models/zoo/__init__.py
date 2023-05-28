1e77c8010e4f6b53d58b36d508a8802b041141d9
Ray Chen <elfynray@hotmail.com>
2023-05-28 17:00:17 +0800

    robust benchmark cause error

--- src/bonner/models/zoo/__init__.py
+++ src/bonner/models/zoo/__init__.py
@@ -1,17 +1,19 @@
 __all__ = [
     "load_pytorch_model",
-    "load_robustbench_model",
+    # "load_robustbench_model",
     "load_vissl_model",
     "load_atlasnet",
     "load_barlow_twins",
     "load_vgg16_places",
     "load_vgg16_robust",
+    "load_model_zoo",
 ]
 
 from bonner.models.zoo._pytorch import load as load_pytorch_model
-from bonner.models.zoo._robustbench import load as load_robustbench_model
+# from bonner.models.zoo._robustbench import load as load_robustbench_model
 from bonner.models.zoo._vissl import load as load_vissl_model
 from bonner.models.zoo._atlasnet import load as load_atlasnet
 from bonner.models.zoo._barlow_twins import load as load_barlow_twins
 from bonner.models.zoo._vgg16_places import load as load_vgg16_places
 from bonner.models.zoo._vgg16_robust import load as load_vgg16_robust
+from bonner.models.zoo._model_zoo import load as load_model_zoo
