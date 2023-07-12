7717a43a5cb00f80b1e92992ab4732eda1104564
zche377 <elfynray@hotmail.com>
2023-07-12 13:44:58 -0400

    temp fix: vit-like models preserve 2 spatial dims

--- src/bonner/models/hooks/_global_maxpool.py
+++ src/bonner/models/hooks/_global_maxpool.py
@@ -4,8 +4,12 @@ from bonner.models.hooks._definition import Hook
 
 
 class GlobalMaxpool(Hook):
-    def __init__(self) -> None:
-        super().__init__(identifier="global_maxpool")
+    def __init__(self, amax_dim: int | list[int] = None) -> None:
+        identifier = "global_maxpool"
+        if amax_dim:
+            identifier += f".amax_dim={amax_dim}".replace(" ", "")
+        super().__init__(identifier=identifier)
+        self.amax_dim = amax_dim
 
     def __call__(self, features: torch.Tensor) -> torch.Tensor:
         """Globally max-pools the features along all spatial dimensions.
@@ -13,6 +17,7 @@ class GlobalMaxpool(Hook):
         WARNING: this function assumes that
 
             * all 4-D features are from convolutional layers and have the shape ``(presentation, channel, spatial_x, spatial_y)``
+            * UPDATE: exception of 4-D features: VITs and convnext from torchvision have shape (presentation, *, *, channel)
             * all 3-D features are from patch-based Vision Transformers and have the shape ``(presentation, patch, channel)``
 
         Args:
@@ -21,6 +26,11 @@ class GlobalMaxpool(Hook):
         Returns:
             spatially max-pooled features
         """
+        # for edge cases noted above
+        if self.amax_dim:
+            assert features.ndim == 4
+            return features.amax(dim=self.amax_dim)
+        
         match features.ndim:
             case 4:  # normal conv maxpool
                 return features.amax(dim=[-2, -1])
