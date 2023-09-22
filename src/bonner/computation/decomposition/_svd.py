fix(svd): make SVD use numpy (GESVD) for precision

src/bonner/computation/decomposition/_svd.py
@@ -1,3 +1,4 @@
+import numpy as np
 import torch
 
 
@@ -8,12 +9,18 @@ def svd(
     n_components: int,
     seed: int,
 ) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
-    if truncated:
+    if (x.get_device() != -1) and truncated:
         torch.manual_seed(seed)
         u, s, v = torch.pca_lowrank(x, center=False, q=n_components)
         v_h = v.transpose(-2, -1)
+        del v
+        torch.cuda.empty_cache()
     else:
-        u, s, v_h = torch.linalg.svd(x, full_matrices=False, driver="gesvd")
+        u, s, v_h = np.linalg.svd(x.numpy(), full_matrices=False)
+        u = torch.from_numpy(u)
+        s = torch.from_numpy(s)
+        v_h = torch.from_numpy(v_h)
+
     u, v_h = svd_flip(u=u, v=v_h)
     return u, s, v_h
 