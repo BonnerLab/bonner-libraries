9d0fa54e7ff6a003bc844dc6851973a4ede4cee5
Raj Magesh Gauthaman <raj.magesh@protonmail.com>
2023-11-16 18:24:13 -0500

    .

--- src/bonner/datasets/allen2021_natural_scenes/_utilities.py
+++ src/bonner/datasets/allen2021_natural_scenes/_utilities.py
@@ -2,8 +2,6 @@ from collections.abc import Collection, Mapping
 
 import numpy as np
 import xarray as xr
-from bonner.computation.xarray import groupby_reset
-
 from bonner.datasets._utilities import BONNER_DATASETS_HOME
 
 IDENTIFIER = "allen2021.natural_scenes"
@@ -13,45 +11,52 @@ N_SUBJECTS = 8
 
 
 def compute_shared_stimuli(
-    assemblies: Collection[xr.DataArray], *, n_repetitions: int = 1
+    assemblies: Collection[xr.DataArray],
+    *,
+    n_repetitions: int = 1,
 ) -> set[int]:
-    """Gets the IDs of the stimuli shared across all the provided assemblies.
+    """Get the IDs of the stimuli shared across all the provided assemblies.
 
     Args:
+    ----
         assemblies: assemblies for different subjects
         n_repetitions: minimum number of repetitions for the shared stimuli in each subject
 
     Returns:
+    -------
         shared stimulus ids
     """
     try:
         return set.intersection(
             *[
                 set(
-                    assembly["stimulus"].values[
-                        (assembly["repetition"] == n_repetitions - 1).values
-                    ]
+                    assembly["stimulus"].data[
+                        (assembly["repetition"] == n_repetitions - 1).data
+                    ],
                 )
                 for assembly in assemblies
-            ]
+            ],
         )
     except Exception:
         return set.intersection(
-            *[set(assembly["stimulus"].values) for assembly in assemblies]
+            *[set(assembly["stimulus"].values) for assembly in assemblies],
         )
 
 
 def compute_noise_ceiling(
-    stimuli: xr.DataArray, *, ncsnr: xr.DataArray
+    stimuli: xr.DataArray,
+    *,
+    ncsnr: xr.DataArray,
 ) -> xr.DataArray:
-    """Compute the noise ceiling for a subject's fMRI data using the method described
-    in the NSD Data Manual under the "Conversion of ncsnr to noise ceiling percentages"
-    section.
+    """Compute the noise ceiling for a subject's fMRI data using the method described in the NSD Data Manual under the "Conversion of ncsnr to noise ceiling percentages" section.
 
     Args:
-        assembly: a subject's neural data
+    ----
+        stimuli: TODO
+        ncsnr: TODO
 
     Returns:
+    -------
         noise ceilings for all voxels
     """
     groupby = stimuli.groupby("stimulus")
@@ -62,7 +67,7 @@ def compute_noise_ceiling(
         fraction = 1
     else:
         unique, counts = np.unique(counts, return_counts=True)
-        reps = dict(zip(unique, counts))
+        reps = dict(zip(unique, counts, strict=True))
         fraction = (reps[1] + reps[2] / 2 + reps[3] / 3) / (reps[1] + reps[2] + reps[3])
 
     ncsnr_squared = ncsnr**2
@@ -76,9 +81,8 @@ def create_roi_selector(
 ) -> np.ndarray:
     selections = []
     for selector in selectors:
-        selection = rois.sel(selector).values
+        selection = rois.sel(selector).data
         if selection.ndim == 1:
             selection = np.expand_dims(selection, axis=0)
         selections.append(selection)
-    selection = np.any(np.concatenate(selections, axis=0), axis=0)
-    return selection
+    return np.any(np.concatenate(selections, axis=0), axis=0)
