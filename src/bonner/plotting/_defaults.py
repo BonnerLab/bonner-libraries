f2146fb3ceae96138e3564bcf29dd1bb147983dd
Raj Magesh Gauthaman <raj.magesh@protonmail.com>
2024-08-28 17:36:55 -0400

    .

--- src/bonner/plotting/_defaults.py
+++ src/bonner/plotting/_defaults.py
@@ -1,23 +1,37 @@
-from bonner.plotting._fonts import install_newcomputermodern
 from matplotlib.font_manager import get_font_names
 
-install_newcomputermodern()
-if "NewComputerModernMath" in get_font_names():
-    kwargs = {
-        "font.serif": ["NewComputerModernMath"],
-        "font.sans-serif": ["NewComputerModernSans10"],
-    }
-else:
-    kwargs = {}
+from bonner.plotting._fonts import install_newcomputermodern
 
-DEFAULT_MATPLOTLIBRC = kwargs | {
+DEFAULT_FONTS = {
     "font.family": ["sans-serif", "serif"],
-    "font.size": 10,
     "mathtext.fontset": "cm",
     "axes.formatter.use_mathtext": True,
     "pdf.fonttype": 42,
     "svg.fonttype": "none",
+}
+
+install_newcomputermodern()
+if "NewComputerModernMath" in get_font_names():
+    DEFAULT_FONTS |= {
+        "font.serif": ["NewComputerModernMath"],
+        "font.sans-serif": ["NewComputerModernSans10"],
+    }
+
+DEFAULT_SIZES = {
+    "font.size": 10,
     "figure.figsize": (3, 3),
+    "axes.labelsize": "medium",
+    "figure.titlesize": "medium",
+    "figure.labelsize": "medium",
+    "legend.title_fontsize": "small",
+    "legend.fontsize": "x-small",
+    "xtick.major.size": 3.5,
+    "xtick.major.width": 1,
+    "ytick.major.size": 3.5,
+    "ytick.major.width": 1,
+}
+
+DEFAULT_FIGURE_OPTIONS = {
     "figure.autolayout": True,
     "savefig.dpi": 600,
     "savefig.bbox": "tight",
@@ -26,23 +40,37 @@ DEFAULT_MATPLOTLIBRC = kwargs | {
     "savefig.transparent": True,
     "figure.facecolor": "None",
     "axes.facecolor": "None",
-    "axes.labelsize": "medium",
     "axes.spines.top": False,
     "axes.spines.right": False,
     "legend.edgecolor": "None",
-    "figure.titlesize": "medium",
-    "figure.labelsize": "medium",
-    "legend.title_fontsize": "small",
-    "legend.fontsize": "x-small",
     "legend.fancybox": False,
     "legend.frameon": False,
     "xtick.minor.visible": False,
     "ytick.minor.visible": False,
-    "xtick.major.size": 3.5,
-    "xtick.major.width": 1,
-    "ytick.major.size": 3.5,
-    "ytick.major.width": 1,
     "xtick.minor.size": 0,
     "ytick.minor.size": 0,
     "patch.linewidth": 0,
+}
+DEFAULT_MATPLOTLIBRC = DEFAULT_FONTS | DEFAULT_SIZES | DEFAULT_FIGURE_OPTIONS
+
+MM_PER_PT = 0.35277777777778
+PT_PER_MM = 1 / MM_PER_PT
+THICKNESS_IN_MM = 1.5
+THICKNESS_IN_PTS = THICKNESS_IN_MM * PT_PER_MM
+
+POSTER_SIZES = {
+    "axes.linewidth": THICKNESS_IN_PTS,
+    "xtick.major.width": THICKNESS_IN_PTS,
+    "ytick.major.width": THICKNESS_IN_PTS,
+    "axes.labelpad": 20,
+    "axes.titlepad": 20,
+    "xtick.major.pad": 16,
+    "ytick.major.pad": 16,
+    "font.size": 32,
+    "axes.labelsize": 48,
+    "axes.titlesize": 48,
+    "xtick.labelsize": 36,
+    "ytick.labelsize": 36,
+    "legend.fontsize": 36,
+    "legend.borderaxespad": 0,
 }
