e3a9d3f7e78f4041b6726d0fb195fe80d2067ae2
Raj Magesh Gauthaman <raj.magesh@protonmail.com>
2024-10-30 22:43:02 -0400

    fix: curv_map overwritten

--- src/bonner/plotting/_nilearn.py
+++ src/bonner/plotting/_nilearn.py
@@ -1,13 +1,15 @@
 import numpy as np
+import numpy.typing as npt
 
 
 def normalize_curv_map(
-    curv_map,
+    curv_map: npt.NDArray[np.floating],
     /,
     *,
     low: float = 0.25,
     high: float = 0.5,
 ) -> np.ndarray:
+    curv_map = curv_map.copy()
     negative = curv_map < 0
     positive = curv_map >= 0
     curv_map[negative] = low
